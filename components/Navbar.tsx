bafa99d870a47939f7fa36428a70f71eba4e4803
Business <business@Rams-MBP.attlocal.net>
2024-09-19 23:36:15 -0500

    removed pathname from navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,8 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-  const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
